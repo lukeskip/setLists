5fe77310bf6a6c431bb562e381547f191d9be4a5
Sergio Alberto Garcia Gutierrez <cheko@iMac-de-Sergio.local>
2017-04-26 21:06:35 -0500

    cambio en portada

--- resources/views/setlists/portada.blade.php
+++ resources/views/setlists/portada.blade.php
@@ -4,7 +4,7 @@
 	
 	
 	<div class="text-center">
-		<h1>¿Cómo hacer un setlist? Generador de Setlist</h1>
+		<h1>¿Cómo hacer un setlist?</h1>
 		<p>
 			El setlist es una pieza fundamental para cualquier banda, es el guión del espectáculo y debe planearse muy bien y ensayarse días antes de la presentación. Este generador generador es una herramienta que busca ayudar a los músicos en esta tarea, administrando y compartiendo sus Setlists. 
 		</p>
